b4fa7adb8115cb1f33686f4ade0c38c48fd8e508
Joshua Meldrum <jmeldrum@clean-streak.com>
2021-09-16 22:21:41 -0400

    push

---
+++ sql_lab_1.sql
@@ -0,0 +1,9 @@
+#Select movie titles
+SELECT * FROM sakila.film;
+select sakila.film.title
+from sakila.film;
+
+#Get languages/Alias
+SELECT * FROM sakila.language;
+select language_id as Language
+from sakila.language;

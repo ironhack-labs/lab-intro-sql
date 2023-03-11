be20afbfa788bb1c39aa0f53c5765ff190ef7e6e
jcosta92 <115988103+jcosta92@users.noreply.github.com>
2023-03-11 11:52:27 +0000

    Add files via upload

---
+++ lab-intro-sql_Jose Costa.sql
@@ -0,0 +1,14 @@
+use sakila;
+
+select * from actor, film, customer;
+
+select title from film;
+
+select distinct original_language_id from film;
+
+select count(store_id) from store;
+
+select count(staff_id) from staff;
+
+select first_name from staff;
+

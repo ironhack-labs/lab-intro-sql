bbcb3bc768d9ba586070b194671072490e476af7
brunofernandes <btdjfernandes@gmail.com>
2023-11-20 19:48:37 +0000

    [lab-intro-sql] Bruno

---
+++ Lab Intro SQL.sql
@@ -0,0 +1,21 @@
+select * from sakila;
+
+select* from sakila.actor;
+select* from sakila.film;
+select * from sakila.customer;
+
+select title from sakila.film;
+
+select distinct name as language from sakila.language;
+
+select count(*) as store
+from sakila.store;
+
+select count(*) as staff
+from sakila.staff;
+
+select first_name as Name 
+from sakila.staff;
+
+
+
